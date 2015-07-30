e94483aa48f71de7b425d705be9747725c143c09
Ke Liao <liaoke@gmail.com>
2015-07-29 17:27:38 -0700

    end of the day commit july 29 2015

---
+++ README.md
@@ -0,0 +1,1 @@
+project for book: beginning amazon web services with node.js
--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(function(req, res, next) {
 // error handlers
 
 // development error handler
-// will print stacktrace
+// will print stack trace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
